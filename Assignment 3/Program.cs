16f6dc4825fce22135dd67ba6b9e1c84e0822658
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-04-21 12:23:17 +0200

    first commit

---
+++ Assignment 3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter number to convert to words");
+            string number;
+            Words convert = new Words();
+            number = convert.NumbWords(int.Parse(Console.ReadLine()));
+            Console.WriteLine(number);
+        }
+    }
+}
